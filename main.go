d5246061df01c2da84ba593b786b5c8713572f8f
Robert <58073966+RobertVenningbo@users.noreply.github.com>
2022-05-19 11:27:58 +0200

    Update main.go

--- main.go
+++ main.go
@@ -8,26 +8,27 @@ import (
 	"github.com/xlab-si/emmy/crypto/ec"
 )
 
-func main() {
-	curve := ec.P256                  // Define curve
-	group := ec.NewGroup(curve)       // Init group from curve
-	prover := NewProver(curve)        // Agents: prover
-	verifier := NewVerifier(curve)    // Agents: verifier
-	a := common.GetRandomInt(group.Q) // generate random r
-	generator := group.ExpBaseG(a)    // generator g for group
-
-	verifier.SetGenerator(generator)
-	prover.GenerateProofData(generator) // Prover generates random x
-
-	h := prover.GenerateH(generator)          // Prover calculates h = g^x 
-	verifier.SetH(h)                          // Verifier gets h
-	u := prover.GenerateU()                   // Prover calculates u = g^r
-	verifier.SetU(u)                          // Verifier gets u
-	challenge := verifier.GenerateChallenge() // Verifier generates a challenge
-
-	z := prover.GetProofData(challenge) // Prover calculates z = r + cx
-
-	verified := verifier.Verify(z) // Verifier verifies g^z = u * h^c
-	fmt.Println("Verifier verifies that prover knows of x.")
-	fmt.Println("Prover knows of x: ", verified)
+func main() {  
+    curve := ec.P256                    // Define curve
+    group := ec.NewGroup(curve)         // Init group from curve
+    prover := NewProver(curve)          // Agents: prover
+    verifier := NewVerifier(curve)      // Agents: verifier
+    a := common.GetRandomInt(group.Q)   // generate random r
+    generator := group.ExpBaseG(a)      // generator g for group
+    verifier.SetGenerator(generator)
+    prover.GenerateProofData(generator) // Prover generates random x and sets the generator
+    h := prover.GenerateH(generator)           // Prover calculates h = g^x
+    verifier.SetH(h)                          // Verifier gets h
+ 
+ 
+    u := prover.GenerateU()                   // Prover calculates u = g^r
+    verifier.SetU(u)                          // Verifier gets u
+    challenge := verifier.GenerateChallenge() // Verifier generates a challenge
+ 
+    z := prover.GetProofData(challenge) // Prover calculates z = r + cx (mod q)
+ 
+    verified := verifier.Verify(z) // Verifier verifies g^z = u * h^c
+    fmt.Println("Verifier verifies that prover knows of x.")
+    fmt.Println("Prover knows of x: ", verified)
 }
+
